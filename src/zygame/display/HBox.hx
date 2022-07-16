b155eb70f7cf830fc2d543235c7069bc0e8e13e7
Rainy <939029835@qq.com>
2022-07-16 18:53:09 +0800

    Create HBox.hx

---
+++ src/zygame/display/HBox.hx
@@ -0,0 +1,28 @@
+package zygame.display;
+
+import zygame.display.base.IDisplayObject;
+
+/**
+ * 横向排序的Box
+ */
+class HBox extends Box {
+	/**
+	 * 间隔
+	 */
+	public var gap:Float = 0;
+
+	override function layout() {
+		super.layout();
+		// 重新排序
+		var offestX = 0.;
+		for (object in this.children) {
+			object.x = offestX;
+			offestX += gap;
+			if (object is IDisplayObject) {
+				offestX += cast(object, IDisplayObject).width;
+			} else {
+				offestX += object.getSize().width;
+			}
+		}
+	}
+}
