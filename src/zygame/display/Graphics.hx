ac29e62c65b14b9ff5f7ab8981555a659d5b24a5
Rainy <939029835@qq.com>
2022-07-21 21:49:06 +0800

    add Graphics drawTriangles api

--- src/zygame/display/Graphics.hx
+++ src/zygame/display/Graphics.hx
@@ -5,4 +5,21 @@ import zygame.display.base.BaseGraphics;
 /**
  * 图形绘制
  */
-class Graphics extends BaseGraphics {}
+class Graphics extends BaseGraphics {
+	/**
+	 * 绘制三角形
+	 * @param vertices 顶点坐标
+	 * @param indices 顶点顺序
+	 * @param uvtData UV数据
+	 */
+	public function drawTriangles(vertices:Array<Float>, indices:Array<Int> = null, uvtData:Array<Float> = null, alphas:Array<Float> = null):Void {
+		var counts:Int = indices.length;
+		trace(color.r, color.g, color.b);
+		for (i in 0...counts) {
+			var pos = indices[i];
+			var index = pos * 2;
+			this.addVertex(vertices[index], vertices[index + 1], curR, curG, curB, alphas == null ? 1 : alphas[pos], uvtData == null ? 0 : uvtData[index],
+				uvtData == null ? 0 : uvtData[index + 1]);
+		}
+	}
+}
