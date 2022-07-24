e2592567f3672aef1483aeb2791036be99cc8e11
Rainy <939029835@qq.com>
2022-07-24 12:01:38 +0800

    Update Graphics.hx

--- src/zygame/display/Graphics.hx
+++ src/zygame/display/Graphics.hx
@@ -17,15 +17,16 @@ class Graphics extends BaseGraphics {
 	 * @param indices 顶点顺序
 	 * @param uvtData UV数据
 	 */
-	public function drawTriangles(vertices:Array<Float>, indices:Array<Int> = null, uvtData:Array<Float> = null, r:Float = 1, g:Float = 1, b:Float = 1,
-			a:Float = 1):Void {
+	public function drawTriangles(vertices:Array<Float>, indices:Array<Int> = null, uvtData:Array<Float> = null, r:Null<Float> = null, g:Null<Float> = null,
+			b:Null<Float> = null, a:Null<Float> = 1):Void {
 		this.flush();
 		var counts:Int = indices.length;
 		var v = 0;
 		for (i in 0...counts) {
 			var pos = indices[i];
 			var index = pos * 2;
-			this.addVertex(vertices[index], vertices[index + 1], r, g, b, a, uvtData == null ? 0 : uvtData[index], uvtData == null ? 0 : uvtData[index + 1]);
+			this.addVertex(vertices[index], vertices[index + 1], r == null ? curR : r, g == null ? curG : g, b == null ? curB : b, a,
+				uvtData == null ? 0 : uvtData[index], uvtData == null ? 0 : uvtData[index + 1]);
 			v++;
 			if (v == 3) {
 				this.flush();
